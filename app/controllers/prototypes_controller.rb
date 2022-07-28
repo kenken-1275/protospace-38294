a31fa213ef227002a0c9c2fc564e1543330d2518
kenken-777 <ich.liebe.r.h.u.t.1@gmail.com>
2022-07-28 21:51:34 +0900

    編集するボタンクリック時の遷移変更

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,7 +1,6 @@
 class PrototypesController < ApplicationController
 
   before_action :authenticate_user!, only: [:new,:edit,:destroy]
-  before_action :move_to_index , only: :edit
 
   def index
     @prototypes = Prototype.includes(:user)
@@ -28,6 +27,9 @@ class PrototypesController < ApplicationController
 
   def edit
     @prototype = Prototype.find(params[:id])
+    unless current_user.id == @prototype.user_id
+      redirect_to action: :index
+    end
   end
 
   def update
@@ -49,12 +51,6 @@ class PrototypesController < ApplicationController
   private
   def prototype_params
     params.require(:prototype).permit(:title,:catch_copy,:concept,:image).merge(user_id:current_user.id)
-  end
-
-  def move_to_index
-    unless current_user.id == params[:user_id]
-      redirect_to action: :index
-    end
   end
 
 end
